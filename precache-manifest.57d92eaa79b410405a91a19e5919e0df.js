3e1a14db0de39b5478ffa524b3dd70b0d243c3b9
unknown <st9800@gmail.com>
2020-07-30 00:53:48 +0530

    Updates

---
+++ precache-manifest.57d92eaa79b410405a91a19e5919e0df.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801b4b08f1d35f1d3f37dc27dce1f61d",
+    "url": "/reactshoppingapp/index.html"
+  },
+  {
+    "revision": "c650a402828402c42544",
+    "url": "/reactshoppingapp/static/css/main.59ff7b66.chunk.css"
+  },
+  {
+    "revision": "f007e0b7c9c341bde701",
+    "url": "/reactshoppingapp/static/js/2.5cfc6234.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactshoppingapp/static/js/2.5cfc6234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c650a402828402c42544",
+    "url": "/reactshoppingapp/static/js/main.f5c76ad4.chunk.js"
+  },
+  {
+    "revision": "83b36002873fafd36ad8",
+    "url": "/reactshoppingapp/static/js/runtime-main.1e065350.js"
+  },
+  {
+    "revision": "bf427ea2ec8779ca8e47ccdf8d52d564",
+    "url": "/reactshoppingapp/static/media/KrazyLogoGreen.bf427ea2.png"
+  },
+  {
+    "revision": "a79ea69aed741b080d0180f3a7f5ff35",
+    "url": "/reactshoppingapp/static/media/basket_shopping.a79ea69a.png"
+  },
+  {
+    "revision": "fc7ea2a06be544b29b5475f69b6a7a4d",
+    "url": "/reactshoppingapp/static/media/beet_veg.fc7ea2a0.png"
+  },
+  {
+    "revision": "f3a44526e8aeaa1206d4cac6ebf6aafc",
+    "url": "/reactshoppingapp/static/media/brinjal_veg.f3a44526.png"
+  },
+  {
+    "revision": "c9f189bc1b5d6af159b8aaea89a915d3",
+    "url": "/reactshoppingapp/static/media/carrot_veg.c9f189bc.png"
+  },
+  {
+    "revision": "cd03cff314361d1970f9a74ef02e1554",
+    "url": "/reactshoppingapp/static/media/ginger_veg.cd03cff3.png"
+  },
+  {
+    "revision": "540091b9ed8618aa57dfd183e048556e",
+    "url": "/reactshoppingapp/static/media/lemon_veg.540091b9.png"
+  },
+  {
+    "revision": "ce5a2c2e9ec071880abe0cf09e75b4df",
+    "url": "/reactshoppingapp/static/media/modern_burger.ce5a2c2e.png"
+  },
+  {
+    "revision": "34245866d99f2130862f54e99c4a47c9",
+    "url": "/reactshoppingapp/static/media/template_groceries_mobile.34245866.png"
+  },
+  {
+    "revision": "ab590c55c434c60f0269f651d0da24d6",
+    "url": "/reactshoppingapp/static/media/valor_veg.ab590c55.png"
+  }
+]);
